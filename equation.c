3cbd6c73e1ab85f3460e2ec9e52c79c5b9ee34ea
Geranis Furas <geranisf@gmail.com>
2016-03-25 22:24:39 +0600

    Assigning variables their values

--- equation.c
+++ equation.c
@@ -10,5 +10,16 @@ int main(int argc, char *argv[])
  		exit(EXIT_FAILURE);
  	}
  	
+	double a, b, c, d, x1, x2;
+
+ 	a = atof(argv[1]);
+ 	b = atof(argv[2]); 
+ 	c = atof(argv[3]);
+ 	
+ 	if (a == 0) 
+	{
+ 		fprintf(stderr, "Equation: Coefficient A can't be equal 0!!! Try again!\n");
+ 		exit(EXIT_FAILURE);
+ 	}
  	return EXIT_SUCCESS;
 } 
