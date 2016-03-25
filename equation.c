18e91f11423f00a56f9ecc668875bec916e95bce
Geranis Furas <geranisf@gmail.com>
2016-03-25 22:28:41 +0600

    RELEASE v1.0

--- equation.c
+++ equation.c
@@ -21,5 +21,25 @@ int main(int argc, char *argv[])
  		fprintf(stderr, "Equation: Coefficient A can't be equal 0!!! Try again!\n");
  		exit(EXIT_FAILURE);
  	}
+	
+	d = b * b - 4 * a * c;
+
+	if (d < 0) {
+		fprintf(stderr, "Equation: No real roots!!!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	x1 = (-b - sqrt(d)) / (2 * a);
+	x2 = (-b + sqrt(d)) / (2 * a);
+		
+	if (x1 == x2) 
+	{
+		printf("X = %.3f\n", x1);
+	} 
+	else 
+	{
+		printf("X1 = %.3f ; X2 = %.3f\n", x1, x2);
+	}
+	
  	return EXIT_SUCCESS;
 } 
